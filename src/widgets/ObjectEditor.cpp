c50d18f011e696c79194649d7c04b62a33a38b23
Alessandro Thea <alessandro.thea@cern.ch>
2024-02-11 21:30:27 +0100

    More cleanup

--- src/widgets/ObjectEditor.cpp
+++ src/widgets/ObjectEditor.cpp
@@ -654,9 +654,6 @@ void dbe::ObjectEditor::set_attribute_widget ( dunedaq::oksdbinterfaces::attribu
   {
     QStringList defaults_values
     { dbe::config::api::get::defaults::attribute::value ( Attribute ) };
-    for ( auto qs : defaults_values ) {
-      std::cout << Attribute.p_name << " : " << qs.toStdString() << std::endl;
-    }
     Widget->setdefaults ( defaults_values.at ( 0 ) );
 
     if ( this_is_in_creation_mode) {
