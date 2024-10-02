f3f6242df607caf103de883caca7b029afdab65a
Gordon Crone <g.crone@ucl.ac.uk>
2024-10-02 14:57:42 +0100

    Remove commented debug code

--- src/widgets/ObjectCreator.cpp
+++ src/widgets/ObjectCreator.cpp
@@ -75,9 +75,6 @@ dbe::ObjectCreator::ObjectCreator ( dunedaq::conffwk::class_t const & cinfo, QWi
   int index = ui->ComboBoxClass->findText ( cname );
   ui->ComboBoxClass->setCurrentIndex ( 1 );  // If index is 0 somehow this doesnt work
   ui->ComboBoxClass->setCurrentIndex ( index ); // Now set it to the right value posssibly 0
-  // std::cout << "index=" << index
-  //           << " ui->ComboBoxClass->currentIndex=" << ui->ComboBoxClass->currentIndex()
-  //           << " valid=" << ui->ComboBoxClass->IsValid() << "\n";
   setup_editor();
   ui->ComboBoxClass->setEditText ( cname );
 
