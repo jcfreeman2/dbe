897a2084c66bdaba7eaa667410919cc2b551f733
Gordon Crone <g.crone@ucl.ac.uk>
2025-01-07 17:08:04 +0000

    turd polishing: still can't get automatic placement of multiple connections to work nicely but it can be persuaded to make readable diagrams now (I think)

--- apps/SchemaEditor/SchemaGraphicSegmentedArrow.cpp
+++ apps/SchemaEditor/SchemaGraphicSegmentedArrow.cpp
@@ -85,48 +85,24 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
 
   qreal xoffset;
   qreal yoffset;
-  const qreal factor = 18.0;
+  const qreal xfactor = 23.0;
+  const qreal yfactor = 17.0;
   if (m_start_item->boundingRect().x() < m_end_item->boundingRect().x()) {
-    xoffset = m_connection_count*factor;
+    xoffset = m_connection_count*xfactor;
   }
   else {
-    xoffset = m_connection_count*-factor;
+    xoffset = m_connection_count*-xfactor;
   }
 
-  if (m_start_item->boundingRect().y() < m_end_item->boundingRect().y()) {
-    yoffset = m_connection_count*factor;
+  if (m_start_item->boundingRect().y() > m_end_item->boundingRect().y()) {
+    yoffset = m_connection_count*yfactor;
   }
   else {
-    yoffset = m_connection_count*-factor;
+    yoffset = m_connection_count*-yfactor;
   }
 
-  auto maxy_start_offset = m_start_item->boundingRect().height() / 2;
   QPointF start_offset(xoffset, yoffset);
-  int max_con_start = m_start_item->boundingRect().height() / factor;
-  if (abs(yoffset) > maxy_start_offset) {
-    if (m_connection_count < max_con_start) {
-      auto sign = yoffset/yoffset;
-      start_offset.setY((m_connection_count - max_con_start/2) * sign * factor);
-    }
-    else {
-      start_offset.setY(0.0);
-    }
-  }
-
   QPointF end_offset(xoffset, yoffset);
-  // if (m_end_item->boundingRect().height()/2.0 < abs(yoffset)) {
-  auto maxy_end_offset = m_end_item->boundingRect().height() / 2;
-  int max_con_end = m_end_item->boundingRect().height() / factor;
-  if (abs(yoffset) > maxy_end_offset) {
-    if (m_connection_count < max_con_end) {
-      auto sign = yoffset/yoffset;
-      yoffset = (m_connection_count - max_con_end/2) * sign * factor;
-      end_offset.setY(yoffset);
-    }
-    else {
-      end_offset.setY(0.0);
-    }
-  }
 
   QLineF center_line ( m_start_item->mapToScene ( m_start_item->boundingRect().center()+start_offset ),
                       m_end_item->mapToScene ( m_end_item->boundingRect().center()+end_offset ) );
@@ -137,6 +113,7 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
 
   // Iterate on the sides starting from top-left
   QPointF p1 = end_polygon.first() + m_end_item->pos(), p2;
+  QPointF end_sp;
   for ( int i = 1; i < end_polygon.count(); ++i )
   {
 
@@ -145,6 +122,17 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
     QLineF::IntersectType intersect_type = item_side.intersects ( center_line, &intersect_point_end );
 
     if ( intersect_type == QLineF::BoundedIntersection ) {
+      switch (i) {
+      case 1:
+      case 3: end_sp=QPointF {m_end_item->pos().x()+m_end_item->boundingRect().width() / 2+xoffset,
+        intersect_point_end.y()};
+        break;
+
+      case 2:
+      case 4: end_sp={intersect_point_end.x(),
+          m_end_item->pos().y()+m_end_item->boundingRect().height() / 2+yoffset};
+        break;
+      }
       intersect_norm_end = norms[i-1];
       break;
     }
@@ -153,14 +141,29 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
   }
 
   p1 = start_polygon.first() + m_start_item->pos();
+  QPointF start_sp;
   for ( int i = 1; i < start_polygon.count(); ++i )
   {
     p2 = start_polygon.at ( i ) + m_start_item->pos();
     QLineF item_side = QLineF ( p1, p2 );
     QLineF::IntersectType intersect_type = item_side.intersects ( center_line,
                                                                &intersect_point_start );
-
     if ( intersect_type == QLineF::BoundedIntersection ) {
+      switch (i) {
+      case 1:
+        start_sp=QPointF {m_start_item->pos().x() + (m_start_item->boundingRect().width() / 2) + xoffset,
+        intersect_point_start.y()};
+        break;
+      case 3:
+        start_sp=QPointF {m_start_item->pos().x() + (m_start_item->boundingRect().width() / 2) - xoffset,
+        intersect_point_start.y()};
+        break;
+
+      case 2:
+      case 4: start_sp={intersect_point_start.x(),
+          m_start_item->pos().y()+m_start_item->boundingRect().height() / 2+yoffset};
+        break;
+      }
       intersect_norm_start = norms[i-1];
       break;
     }
@@ -168,7 +171,7 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
     p1 = p2;
   }
 
-  QLineF direct_line(intersect_point_start, intersect_point_end);
+  QLineF direct_line(start_sp, end_sp);
   auto label_br = QRectF(QFontMetrics ( m_label_font ).boundingRect ( m_name ));
   // Center rectangle on origin
   label_br.translate(-label_br.width()/2, label_br.height()/2);
@@ -188,8 +191,10 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
 
   if ((intersect_norm_start.dx() == 0) && (intersect_norm_end.dx() == 0)) {
     // Three segments, starting and ending vertically
-    QPointF wp1 = QPointF(direct_line.x1(), direct_line.center().y());
-    QPointF wp2 = QPointF(direct_line.x2(), direct_line.center().y());
+    QPointF wp1 = QPointF(direct_line.x1(),
+                          direct_line.center().y()+yoffset);
+    QPointF wp2 = QPointF(direct_line.x2(),
+                          direct_line.center().y()+yoffset);
     path.moveTo(direct_line.x1(), direct_line.y1());
     path.lineTo(wp1);
     path.lineTo(wp2);
@@ -211,8 +216,10 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
 
   } else if ((intersect_norm_start.dy() == 0) && (intersect_norm_end.dy() == 0)) {
     // Three segments, starting and ending horizontally
-    QPointF wp1 = QPointF(direct_line.center().x(), direct_line.y1());
-    QPointF wp2 = QPointF(direct_line.center().x(), direct_line.y2());
+    QPointF wp1 = QPointF(direct_line.center().x()-xoffset,
+                          direct_line.y1());
+    QPointF wp2 = QPointF(direct_line.center().x()-xoffset,
+                          direct_line.y2());
 
     path.moveTo(direct_line.x1(), direct_line.y1());
     path.lineTo(wp1);
@@ -253,11 +260,12 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
     );
 
   } else if (intersect_norm_start.dy() == 0) {
+    // Two segments, starting horizontally, ending vertically
     QPointF wp1 = QPointF(direct_line.x2(), direct_line.y1() );
     path.moveTo(direct_line.x1(), direct_line.y1());
 
     path.lineTo(wp1);
-    path.translate (0, + m_connection_count*10.0);
+
 
     label_br.translate( wp1 
       + QPointF(
@@ -274,7 +282,7 @@ void SchemaGraphicSegmentedArrow::UpdatePosition()
 
   }
 
-  path.lineTo( intersect_point_end );
+  path.lineTo( end_sp );
   setPath(path);
   
   m_rel_label_br = label_br;
